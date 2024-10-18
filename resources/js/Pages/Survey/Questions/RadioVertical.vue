94fb97e84c83b7704ac18ffcf32ef754696ca89c
Madhukar Prabhakara <msprabhakara@gmail.com>
2024-10-17 22:03:30 -0700

    Vertical radio implementation v1 working

--- resources/js/Pages/Survey/Questions/RadioVertical.vue
+++ resources/js/Pages/Survey/Questions/RadioVertical.vue
@@ -14,24 +14,35 @@
 -->
 <template>
   <div class="mb-6">
-    <label class="text-sm font-bold text-gray-900">How satisfied are you with the lessons taught by the girls code academy?</label>
-    <!--<p class="text-sm text-gray-500">How do you prefer to receive notifications?</p>-->
-    <fieldset class="mt-2">
-      <legend class="sr-only">Notification method</legend>
-      <div class="space-y-4 ml-2">
-        <div v-for="notificationMethod in notificationMethods" :key="notificationMethod.id" class="flex items-center">
-          <input :id="notificationMethod.id" name="notification-method" type="radio" :checked="notificationMethod.id === 'email'" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-          <label :for="notificationMethod.id" class="ml-3 block text-sm font-medium leading-6 text-gray-900">{{ notificationMethod.title }}</label>
+    <label class="text-md font-bold text-gray-900">{{props.qid.question_details.title}}</label>
+    
+      <div class="mt-2 ml-2 space-y-2">
+        <div v-for="option_id in props.qid.question_option_ids" key="option_id.id" class="flex items-center">
+          <input :id="'s_'+option_id.id" :name="option_id.id" v-model="store.fields['a_'+props.qid.id]"  :value="option_id.option_detail.question_option_id" key="option_id.id" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
+          <label :for="'s_'+option_id.id" class="ml-3 block text-sm font-normal leading-6 text-gray-900">{{ option_id.option_detail.title }}</label>
         </div>
       </div>
-    </fieldset>
+   
   </div>
 </template>
 
 <script setup>
+import { ref, watch } from 'vue'
+import { useSurveyStore } from '@/Pages/Store/surveyStore'
+import InputError from '@/Components/InputError.vue';
+const props = defineProps(['qid'])
+const store = useSurveyStore()
 const notificationMethods = [
   { id: 'email', title: 'Email' },
   { id: 'sms', title: 'Phone (SMS)' },
   { id: 'push', title: 'Push notification' },
 ]
+watch(
+      () => store.fields['a_'+props.qid.id],
+      (newField) => {
+      store.addFormFields('a_'+props.qid.id, `${newField}`)
+        
+      },
+      { deep: true }
+    )
 </script>

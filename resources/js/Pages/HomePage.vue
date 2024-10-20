9ebeeeaf4215a0260e3248c808f1bfbefd773b86
Madhukar Prabhakara <madhu@Madhukars-MacBook-Air.local>
2024-10-19 23:19:07 -0700

    home page changed

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -14,7 +14,7 @@
         </div>
         <div class="text-center">
           <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Open-source survey platform focussed on collecting and analyzing qualitative data.</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Collect PDFs, docs and open-ended responses from survey takers? Spend weeks and months extracting insights from these sources? Stop. This is not just a better way to collect data but a radically different one that will turn those months and weeks into probably an hour or two.</p>
+          <p class="mt-6 text-lg leading-8 text-gray-600">Combine the power of survey and qulitative data analysis. Collect PDFs, docs and open-ended responses from survey takers? Spend weeks and months extracting insights from these sources? Stop. This is not just a better way to collect data but a radically different one that will turn those months and weeks into probably an hour or two.</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <!-- <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a> -->
             <a href="#" target="_blank" class="text-sm font-semibold leading-6 text-gray-900">Github link<span aria-hidden="true">→</span></a>

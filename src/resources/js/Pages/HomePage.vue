26cb0e81c82ee96a518366280899284efc12a9ad
Madhukar Prabhakara <madhu@Madhukars-MacBook-Air.local>
2024-11-03 21:33:46 -0800

    Github link

--- src/resources/js/Pages/HomePage.vue
+++ src/resources/js/Pages/HomePage.vue
@@ -18,7 +18,7 @@
           <p class="mt-6 text-lg leading-8 text-gray-600">Collect PDFs, docs and open-ended responses from survey takers? Use multiple tools and spend weeks to extract insights from these sources? Stop. This is not just a better way to collect data but a radically different one that will turn those months and weeks into minutes. The data is analyzed as it comes in.</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <!-- <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a> -->
-            <a href="#" target="_blank" class="text-sm font-semibold leading-6 text-gray-900">Github link<span aria-hidden="true">→</span></a>
+            <a href="https://github.com/Madhukarsprabhakara/seasonsurvey" target="_blank" class="text-sm font-semibold leading-6 text-gray-900">Github link<span aria-hidden="true">→</span></a>
           </div>
         </div>
       </div>

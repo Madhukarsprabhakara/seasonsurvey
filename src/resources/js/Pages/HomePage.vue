09cbfe587608f2f68481fa18b9047903ca436e01
Madhukar Prabhakara <msprabhakara@Madhukars-MacBook-Pro.local>
2024-11-18 16:52:24 -0800

    Home page messaging changes

--- src/resources/js/Pages/HomePage.vue
+++ src/resources/js/Pages/HomePage.vue
@@ -14,7 +14,7 @@
           </div>
         </div>
         <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Survey platform with qualitative data analysis capabilities!</h1>
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Open-source survey platform focussed on qualitative data analytics!</h1>
           <p class="mt-6 text-lg leading-8 text-gray-600">Collect PDFs, docs and open-ended responses from survey takers? Use multiple tools and spend weeks to extract insights from these sources? Stop. This is not just a better way to collect data but a radically different one that will turn those months and weeks into minutes. Survey platform with fantastic qualitative data analysis capabilities.</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <!-- <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a> -->
